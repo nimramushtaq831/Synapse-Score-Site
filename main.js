d2ec1d019e46ef11e2556f8d19639f5df54f33e9
Nimra Mushtaq <nimramushtaq047@gmail.com>
2025-08-22 16:57:29 +0500

    Designed Skills Recommendation
    commit update

--- main.js
+++
@@ -1,8 +0,0 @@
- document.querySelectorAll("#roleMenu li").forEach(item => {
-    item.addEventListener("click", () => {
-      let target = item.getAttribute("data-url");
-      if(target) {
-        window.location.href = target; // Redirect to new page
-      }
-    });
-  });
